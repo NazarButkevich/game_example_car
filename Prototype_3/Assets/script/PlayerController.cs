54ad5f3ea1214b6447e12cd374a5a0f62dc93a71
NazarBukevich <harleyquinnj06@gmail.com>
2021-02-16 11:07:34 +0200

    cookie fire

--- Prototype_3/Assets/script/PlayerController.cs
+++ Prototype_3/Assets/script/PlayerController.cs
@@ -33,6 +33,7 @@ public class PlayerController : MonoBehaviour
 
         if (Input.GetKeyDown(KeyCode.Space))
             {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
             // launch a projectile from the player
         }
     }
