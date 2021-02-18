0bc5af727cc2261b5883e2c54e1e5ee0e4c55564
NazarBukevich <harleyquinnj06@gmail.com>
2021-02-18 12:28:55 +0200

    Destroy objects on collision

--- Prototype_3/Assets/Scripts/DetectCollisions.cs
+++ Prototype_3/Assets/Scripts/DetectCollisions.cs
@@ -15,4 +15,11 @@ public class DetectCollisions : MonoBehaviour
     {
         
     }
+
+
+   private void OnTriggerEnter(Collider other)
+    {
+        Destroy(gameObject);
+        Destroy(other.gameObject);
+    }
 }
