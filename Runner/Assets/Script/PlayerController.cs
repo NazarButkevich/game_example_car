862f9eb5f33195d46b084f8b8c60ef01e04ed56e
NazarBukevich <harleyquinnj06@gmail.com>
2021-02-25 09:26:14 +0200

    Play the particle on collision

--- Runner/Assets/Script/PlayerController.cs
+++ Runner/Assets/Script/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     private Rigidbody playerRb;
     private Animator playerAnim;
+    public ParticleSystem explosionParticle;
     public float jumpForce;
     public float gravityModifier;
     public bool isOnGround = true;
@@ -40,6 +41,7 @@ public class PlayerController : MonoBehaviour
             gameOver = true;
             playerAnim.SetBool("Death_b", true);
             playerAnim.SetInteger("DeathType_int", 1);
+            explosionParticle.Play();
         }
 }
     }
